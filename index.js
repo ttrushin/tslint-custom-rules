0fdb3965cee340b8558ed01b8ad7aca256f29535
Tyler <10791881+ttrushin@users.noreply.github.com>
2018-02-28 10:23:33 -0500

    Merge Development into Master (#7)
    * v1.0.2

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const path = require("path");
+
 module.exports = {
-  rulesDirectory: "dist/"
+  rulesDirectory: path.join(
+    path.dirname(require.resolve("tslint-custom-rules")),
+    "dist/"
+  )
 };
